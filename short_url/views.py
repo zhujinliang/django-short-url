61ca7f6114d1ec0268a9aaa8ec72b060add19a65
Jinliang Zhu <zhujinlianghust@gmail.com>
2014-04-15 11:15:52 +0800

    [REV] Delete some dependency

--- short_url/views.py
+++ short_url/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from django.core.urlresolvers import reverse
 from django.views.generic import RedirectView
 
 from short_url import ShortURL
@@ -21,8 +20,7 @@ class ShortURLRedirectView(RedirectView):
     def get_redirect_url(self, **kwargs):
         short_url = kwargs.get('short_url', '')
         short_url = short_url.replace(SHORT_URL_PREFIX, '')
-        print '=============', short_url
-        long_url = reverse('home')
+        long_url = '/'
         if short_url:
             url = ShortURL.get_long_url(short_url)
             if url:
